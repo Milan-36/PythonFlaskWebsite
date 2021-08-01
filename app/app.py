3a408dce3a713a982ad7138738b105880da3768c
MilanPatel3628 <mkp6@njit.edu>
2021-08-01 05:41:44 -0400

    Feature: Modified app.py with Markup, render_template, make_response, request, jsonify imports

--- app/app.py
+++ app/app.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, Markup, render_template, make_response, request, jsonify
 from logic import square_of_number_plus_nine
 
 
 # Create Flask's `app` object
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
-    return "Hello World!"
 
 # Create Flask's `app` object
 app = Flask(
@@ -20,5 +16,43 @@ app = Flask(
 
 @app.route("/")
 def hello():
+    return "Hello World!"
+
+@app.route("/")
+def logic():
     value = square_of_number_plus_nine(5)
     return value
+
+@app.route("/")
+def Markup():
+    return Markup("<h1>Hello World!</h1>")
+
+@app.route("/")
+def render_template():
+    return render_template("index.html")
+
+@app.route("/response")
+def response():
+    headers = {"Content-Type": "application/json"}
+    return make_response('it worked!', 200, headers)
+
+
+@app.route("/get", methods=['GET'])
+def get_hello():
+    if request.method != 'GET':
+        return make_response('Malformed request', 400)
+    headers = {"Content-Type": "application/json"}
+    return make_response('it worked!', 200, headers)
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    if request.method != 'GET':
+        return make_response('Malformed request', 400)
+    my_dict = {'key': 'dictionary value'}
+    headers = {"Content-Type": "application/json"}
+    return make_response(jsonify(my_dict), 200, headers)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
