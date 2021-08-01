e089f45a33c18cd8fcaf57de1bffe081885822cc
MilanPatel3628 <mkp6@njit.edu>
2021-08-01 18:24:21 -0400

    Feature: Updated routes.py

---
+++ app/Route/routes.py
@@ -0,0 +1,15 @@
+# to set up a basic homepage
+
+"""Route declaration."""
+from flask import current_app as app
+from flask import render_template
+
+
+@app.route('/')
+def home():
+    """Landing page."""
+    return render_template(
+        'home.html',
+        title="Jinja Demo Site",
+        description="Smarter page templates with Flask & Jinja."
+    )
