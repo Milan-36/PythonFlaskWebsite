1af9a2c3154069b95d70cbe56f57af40f154d1e6
MilanPatel3628 <mkp6@njit.edu>
2021-08-02 12:33:19 -0400

    Feature: added another routes for unauthorized user

--- app/auth.py
+++ app/auth.py
@@ -85,3 +85,9 @@ def load_user(user_id):
     if user_id is not None:
         return User.query.get(user_id)
     return None
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('auth_bp.login'))
