a5da70b2f512e4a044974c9a0b57e263a0016cf9
MilanPatel3628 <mkp6@njit.edu>
2021-08-02 11:53:55 -0400

    Task: auth.py signup form

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,6 @@
 """Routes for user authentication."""
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+from .forms import SignupForm
 
 
 # Blueprint Configuration
@@ -10,11 +11,27 @@ auth_bp = Blueprint(
 )
 
 
-@auth_bp.route('/login', methods=['GET', 'POST'])
+@auth_bp.route('/actors/login', methods=['GET', 'POST'])
 def login():
     # Login route logic goes here
 
 
 @auth_bp.route('/signup', methods=['GET', 'POST'])
 def signup():
-    # Signup logic goes here
+    """
+    User sign-up page.
+
+    GET requests serve sign-up page.
+    POST requests validate form & user creation.
+    """
+    form = SignupForm()
+    if form.validate_on_submit():
+        # User sign-up logic will go here.
+        ...
+    return render_template(
+        '/signup.html',
+        title='Create a new Account',
+        form=SignupForm(),
+        template='signup-page',
+        body="Enter Email, and a new password to create an account"
+    )
