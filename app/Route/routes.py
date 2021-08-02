a87e095f31173a7d0e919e7e99e93add4f2ff546
MilanPatel3628 <mkp6@njit.edu>
2021-08-01 20:30:28 -0400

    task: Created routes.py and navigation.html

--- app/Route/routes.py
+++ app/Route/routes.py
@@ -10,6 +10,21 @@ def home():
     """Landing page."""
     return render_template(
         'home.html',
+        title="Flask Application",
+        description="IS601 Project 4."
+    )
+
+@app.route('/')
+def home():
+    """Landing page."""
+    nav = [
+        {'name': 'Home', 'url': 'https://example.com/1'},
+        {'name': 'About', 'url': 'https://example.com/2'},
+        {'name': 'Pics', 'url': 'https://example.com/3'}
+    ]
+    return render_template(
+        'home.html',
+        nav=nav,
         title="Jinja Demo Site",
         description="Smarter page templates with Flask & Jinja."
     )
